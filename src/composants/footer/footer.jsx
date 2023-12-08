3d746154f94cbf764db80cc3e6bdde581a98b23e
axido545 <axido545@gmail.com>
2023-12-08 22:52:27 +0100

    modif header

--- src/composants/footer/footer.jsx
+++ src/composants/footer/footer.jsx
@@ -3,10 +3,10 @@ import logo from "../../assets/logo-kasa-footer.png"
 
 
 function Footer(){
-    return<div className="footer-block">
+    return<footer className="footer-block">
         <img src={logo}/>
 <p>© 2020 Kasa. All rights reserved</p>
-    </div>
+    </footer>
 }
 
 export default Footer;
--- src/composants/header/Header.jsx
+++ src/composants/header/Header.jsx
@@ -1,12 +1,17 @@
-import { NavLink } from "react-router-dom"
-
-export function Header(){
+import logoHeader from '../../assets/logo-kasa-header-desktop.png'
+import './header.css'
+ function Header(){
     return<header>
-    <nav>
-    <NavLink to="/">Accueil</NavLink>
-      <NavLink to="/about">A propos</NavLink>
-      <NavLink to ="/logement">Logement</NavLink>
+        <img className='logo-header' src={logoHeader} />
+   <nav className='nav-header'>
+    <ul>
+        <li>Accueil</li>
+        <li>A propos</li>
+
+    </ul>
+
     </nav>
   </header>
 }
 
+export default Header;
