a99ce101d309d53f5e2488b83bcea94695012781
axido545 <axido545@gmail.com>
2023-12-10 16:41:32 +0100

    add underline nav

--- src/composants/header/Header.jsx
+++ src/composants/header/Header.jsx
@@ -1,17 +1,19 @@
 import logoHeader from '../../assets/logo-kasa-header-desktop.png'
 import './header.css'
+import { NavLink } from 'react-router-dom';
  function Header(){
     return<header>
         <img className='logo-header' src={logoHeader} />
-   <nav className='nav-header'>
-    <ul> <li>
-    <a href={`/`}>Accueil</a>
-    </li>
-<li>
-<a href={`/about`}>A propos</a>
-</li>
-    </ul>
-    </nav>
+            <nav className='nav-header'>
+              <ul>
+                <li>
+                  <NavLink to="/">Accueil</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/about">A propos</NavLink>
+              </li>
+              </ul>
+            </nav>
   </header>
 }
 
