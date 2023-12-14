ed0ead1998b06a738a1e84ad4817333cb5c850a3
axido545 <axido545@gmail.com>
2023-12-14 20:08:35 +0100

    overlay housing home

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,27 +1,27 @@
-import "./article.css"
+import './article.css'
 import PropTypes from 'prop-types'
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 /**
- * 
+ *
  * @argument
  * */
 
-export default function Article({id,title,cover}){
-
-    return <div id={id} className="article-block">
+export default function Article({ id, title, cover }) {
+  return (
+    <div id={id} className="article-block">
       <Link to={`/housing/${id}`}>
-      <img src={cover} alt={title} className="img-article" />
+        <div className="article-overlay"></div>
+
+        <img src={cover} alt={title} className="img-article" />
       </Link>
-        <p className="article-p">{title}</p>
+      <p className="article-p">{title}</p>
     </div>
+  )
 }
 
-
 Article.propTypes = {
-     id: PropTypes.string,
+  id: PropTypes.string,
   cover: PropTypes.string,
   title: PropTypes.string,
-
-
 }
---
+++ src/components/collapse/Collapse.jsx
@@ -0,0 +1,7 @@
+export default function Collapse({ title }) {
+  return (
+    <div className="wrapper-collapse">
+      <div className="inner-collapse"></div>
+    </div>
+  )
+}
