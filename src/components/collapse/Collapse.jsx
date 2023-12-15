89933ed58de383c06291d85a8d0f36241281e42d
axido545 <axido545@gmail.com>
2023-12-15 17:49:47 +0100

    update about  desktop & mobile

--- src/components/collapse/Collapse.jsx
+++ src/components/collapse/Collapse.jsx
@@ -1,16 +1,27 @@
 import PropTypes from 'prop-types'
 import './collapse.css'
+import { useState } from 'react'
 
 export default function Collapse({ title, content }) {
+  const [active, setActive] = useState(false)
+  const handleToggle = () => {
+    setActive(!active)
+  }
   return (
-    <div className="wrapper-collapse">
-      <div className="collapse-title">
+    <div className={`wrapper-collapse ${active && 'active'}`}>
+      <div className="collapse-title" onClick={handleToggle}>
         {title}
         <span>
-          <i class="fa-solid fa-chevron-up chevron-collapse"></i>
+          <i
+            className={`wrapper-collapse chevron-collapse fa-solid ${
+              active ? 'fa-chevron-up' : 'fa-chevron-down'
+            }`}
+          ></i>{' '}
         </span>
       </div>
-      <div className="collapse-content">{content}</div>
+      <div className="collapse-content">
+        <p className="collapse-p">{content}</p>
+      </div>
     </div>
   )
 }
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,16 +3,31 @@ import Footer from '../../components/footer/footer'
 import Banner from '../../components/banner/Banner'
 import bannerAbout from '../../assets/banner-about.png'
 import Collapse from '../../components/collapse/Collapse'
+import './about.css'
 
 function About() {
+  const content1 =
+    'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.'
+
+  const content2 =
+    'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'
+
+  const content3 =
+    "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+
+  const content4 =
+    "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons ségalement des ateliers sur la sécurité domestique pour nos hôtes."
+
   return (
     <>
       <Header />
       <Banner url={bannerAbout} mytitle="" />
-      <Collapse title="Fiabilité" content="mon contenu 1" />
-      <Collapse title="Respect" content="mon contenu 2" />
-      <Collapse title="Service" content="mon contenu 3" />
-      <Collapse title="Sécurité" content="mon contenu 4" />
+      <div className="collapses">
+        <Collapse title="Fiabilité" content={content1} />
+        <Collapse title="Respect" content={content2} />
+        <Collapse title="Service" content={content3} />
+        <Collapse title="Sécurité" content={content4} />
+      </div>
 
       <Footer />
     </>
